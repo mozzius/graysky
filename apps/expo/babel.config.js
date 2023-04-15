5256e7107939bfdcd1b775ba6504f276d7f4e1fe
Samuel Newman <mozzius@protonmail.com>
2023-04-15 18:45:28 +0100

    Initial commit

---
+++ apps/expo/babel.config.js
@@ -0,0 +1,13 @@
+/** @type {import("@babel/core").ConfigFunction} */
+module.exports = function (api) {
+  api.cache.forever();
+
+  // Make Expo Router run from `src/app` instead of `app`.
+  // Path is relative to `/node_modules/expo-router`
+  process.env.EXPO_ROUTER_APP_ROOT = "../../apps/expo/src/app";
+
+  return {
+    plugins: ["nativewind/babel", require.resolve("expo-router/babel")],
+    presets: ["babel-preset-expo"],
+  };
+};
