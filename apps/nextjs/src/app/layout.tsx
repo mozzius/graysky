e4b1c756b24c50121003e50ab34c2e53adbc84a2
Samuel Newman <mozzius@protonmail.com>
2023-05-02 01:02:40 +0100

    remove mention of waitlist

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -3,10 +3,10 @@ import "~/styles/globals.css";
 
 export const metadata = {
   title: "Graysky - a bluesky client",
-  description: "Experience a whole different skyline. Join the waitlist.",
+  description: "Experience a whole different skyline.",
   openGraph: {
     title: "Graysky - a bluesky client",
-    description: "Experience a whole different skyline. Join the waitlist.",
+    description: "Experience a whole different skyline.",
     type: "website",
     locale: "en_GB",
     url: "https://graysky.app",
@@ -22,7 +22,7 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Graysky - a bluesky client",
-    description: "Experience a whole different skyline. Join the waitlist.",
+    description: "Experience a whole different skyline.",
     images: [
       {
         url: "https://graysky.app/graysky.png",
