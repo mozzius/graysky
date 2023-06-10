ac81b45a33c893ce9f1609c352b1152fde00cca1
Samuel Newman <mozzius@protonmail.com>
2023-06-10 13:06:04 +0100

    fix rich text in translations

--- apps/expo/src/components/translation.tsx
+++ apps/expo/src/components/translation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useEffect } from "react";
 import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import { Image } from "expo-image";
@@ -19,6 +20,7 @@ export const Translation = ({ text, uri }: Props) => {
 
   useEffect(() => {
     translate.reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [uri]);
 
   switch (translate.status) {
@@ -51,7 +53,7 @@ export const Translation = ({ text, uri }: Props) => {
       return (
         <View className="mt-1.5 rounded-lg border border-blue-300 bg-blue-50 px-3 py-2 dark:border-blue-700 dark:bg-blue-950">
           <Text style={{ color: theme.colors.text }} className="text-base">
-            {translate.data.text}
+            <RichTextWithoutFacets text={translate.data.text} />
           </Text>
           <View className="mt-1 flex-row items-center justify-between">
             <View className="flex-row items-center">
@@ -60,7 +62,7 @@ export const Translation = ({ text, uri }: Props) => {
                 className="mr-1 text-neutral-500 dark:text-neutral-200"
               />
               <Text className="text-sm text-neutral-500 dark:text-neutral-200">
-                <RichTextWithoutFacets text={translate.data.language} />
+                {translate.data.language}
               </Text>
             </View>
             <Image
@@ -69,6 +71,7 @@ export const Translation = ({ text, uri }: Props) => {
                   ? require("../../assets/translated_by-white.png")
                   : require("../../assets/translated_by.png")
               }
+              alt="Translated by Google"
               style={{ aspectRatio: 7.6 }}
               className="w-28 max-w-full"
             />
