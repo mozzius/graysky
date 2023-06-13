e2d51013e4289261bd9ba4e64c6ae6f69bb5eed6
Samuel Newman <mozzius@protonmail.com>
2023-06-13 08:56:55 +0100

    use lande for lang detection

--- packages/api/src/router/translate.ts
+++ packages/api/src/router/translate.ts
@@ -1,6 +1,164 @@
+import lande, { type Language as Language3 } from "lande";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
+
+type Language2 =
+  | "af"
+  | "ar"
+  | "az"
+  | "be"
+  | "bn"
+  | "bg"
+  | "ca"
+  | "cs"
+  | "ku"
+  | "zh"
+  | "da"
+  | "de"
+  | "el"
+  | "en"
+  | "et"
+  | "eu"
+  | "fi"
+  | "fr"
+  | "ha"
+  | "he"
+  | "hi"
+  | "hr"
+  | "hu"
+  | "hy"
+  | "id"
+  | "is"
+  | "it"
+  | "ja"
+  | "ka"
+  | "kk"
+  | "ko"
+  | "lt"
+  | "mr"
+  | "mk"
+  | "nl"
+  | "nb"
+  | "fa"
+  | "pl"
+  | "pt"
+  | "ro"
+  | "rn"
+  | "ru"
+  | "sk"
+  | "es"
+  | "sr"
+  | "sv"
+  | "tl"
+  | "tr"
+  | "uk"
+  | "vi";
+
+function iso639_3toIso639_2(string: Language3): Language2 {
+  switch (string) {
+    case "afr":
+      return "af";
+    case "ara":
+      return "ar";
+    case "aze":
+      return "az";
+    case "bel":
+      return "be";
+    case "ben":
+      return "bn";
+    case "bul":
+      return "bg";
+    case "cat":
+      return "ca";
+    case "ces":
+      return "cs";
+    case "ckb":
+      return "ku";
+    case "cmn":
+      return "zh";
+    case "dan":
+      return "da";
+    case "deu":
+      return "de";
+    case "ell":
+      return "el";
+    case "eng":
+      return "en";
+    case "est":
+      return "et";
+    case "eus":
+      return "eu";
+    case "fin":
+      return "fi";
+    case "fra":
+      return "fr";
+    case "hau":
+      return "ha";
+    case "heb":
+      return "he";
+    case "hin":
+      return "hi";
+    case "hrv":
+      return "hr";
+    case "hun":
+      return "hu";
+    case "hye":
+      return "hy";
+    case "ind":
+      return "id";
+    case "isl":
+      return "is";
+    case "ita":
+      return "it";
+    case "jpn":
+      return "ja";
+    case "kat":
+      return "ka";
+    case "kaz":
+      return "kk";
+    case "kor":
+      return "ko";
+    case "lit":
+      return "lt";
+    case "mar":
+      return "mr";
+    case "mkd":
+      return "mk";
+    case "nld":
+      return "nl";
+    case "nob":
+      return "nb";
+    case "pes":
+      return "fa";
+    case "pol":
+      return "pl";
+    case "por":
+      return "pt";
+    case "ron":
+      return "ro";
+    case "run":
+      return "rn";
+    case "rus":
+      return "ru";
+    case "slk":
+      return "sk";
+    case "spa":
+      return "es";
+    case "srp":
+      return "sr";
+    case "swe":
+      return "sv";
+    case "tgl":
+      return "tl";
+    case "tur":
+      return "tr";
+    case "ukr":
+      return "uk";
+    case "vie":
+      return "vi";
+  }
+}
 
 export const translateRouter = createTRPCRouter({
   detect: publicProcedure
@@ -31,29 +189,16 @@ export const translateRouter = createTRPCRouter({
         );
       }
 
-      const res = await fetch(
-        "https://translation.googleapis.com/language/translate/v2/detect",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "X-goog-api-key": process.env.TRANSLATION_KEY as string,
-          },
-          body: JSON.stringify({
-            q: needsDetecting.map((post) => post.text),
-          }),
-        },
-      );
+      const detections = needsDetecting
+        .map((post) => {
+          const detected = lande(post.text);
+          if (!detected[0]) return undefined;
+          if (detected[0][1] < 0.5) return undefined;
+          return [post.uri, iso639_3toIso639_2(detected[0][0])];
+        })
+        .filter(Boolean) as [string, string][];
 
-      if (!res.ok) {
-        console.log(res.status, res.statusText);
-        console.log((await res.json()).error.message);
-        throw new Error("API call to gcloud failed");
-      }
-
-      const {
-        data: { detections },
-      } = await res.json();
+      const newDectections = Object.fromEntries(detections);
 
       const existingDectections = Object.fromEntries(
         cached
@@ -61,22 +206,12 @@ export const translateRouter = createTRPCRouter({
           .filter(([_, language]) => language !== "und"),
       );
 
-      const newDectections = Object.fromEntries(
-        needsDetecting
-          .map((post, index) => [
-            post.uri,
-            detections[index]?.[0]?.language as string | undefined,
-          ])
-          .filter(([_, language]) => language !== "und"),
-      );
-
       await db.translatablePost.createMany({
-        data: Object.entries(newDectections)
-          .filter(([_, language]) => language !== undefined)
-          .map(([uri, language]) => ({
-            uri,
-            language: language as string,
-          })),
+        data: detections.map(([uri, language]) => ({
+          uri,
+          language: language as string,
+        })),
+        skipDuplicates: true,
       });
 
       return {
