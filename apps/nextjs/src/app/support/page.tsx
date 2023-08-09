f6b76ceb8a409d5e5201dd815270a3707d2da629
Samuel Newman <mozzius@protonmail.com>
2023-08-09 02:36:34 +0100

    add support page

---
+++ apps/nextjs/src/app/support/page.tsx
@@ -0,0 +1,15 @@
+export default async function SupportPage() {
+  return (
+    <div className="container mx-auto pt-8">
+      <h1 className="text-2xl">Support</h1>
+      <p className="mt-4">
+        If you need help, email us at{" "}
+        <a className="underline" href="mailto:support@graysky.app">
+          support@graysky.app
+        </a>{" "}
+        with a description of your issue and information about how we can help
+        you.
+      </p>
+    </div>
+  );
+}
