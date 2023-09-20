82a9a0280ee5be4f202d1533f5d0265657c62d61
Samuel Newman <mozzius@protonmail.com>
2023-09-20 14:22:41 +0100

    move eyebrow thingy

--- apps/nextjs/src/app/hero.tsx
+++ apps/nextjs/src/app/hero.tsx
@@ -12,7 +12,7 @@ export const Hero = () => {
   return (
     <div className="container z-10 mx-auto flex h-full max-w-4xl flex-row items-center gap-4 p-4">
       <div className="flex w-[calc(50%-8px)] flex-col gap-6">
-        <span className="w-max rounded-full border border-neutral-500 bg-neutral-900 px-4 py-1 text-xs text-white">
+        <span className="w-max translate-y-2 rounded-full border border-neutral-500 bg-neutral-900 px-4 py-1 text-xs text-white">
           Version 0.1.4 available now!
         </span>
         <Balancer as="h1" className="text-5xl font-bold">
