60f062a40db67ae3e05d31d9e77d61d545e721ea
Samuel Newman <mozzius@protonmail.com>
2023-10-05 12:38:35 +0100

    update deps

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,23 +1,8 @@
-import mdx from "@next/mdx";
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds and Linting.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-
-const withMDX = mdx({
-  // Optionally provide remark and rehype plugins
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -33,4 +18,4 @@ const config = {
   },
 };
 
-export default withMDX(config);
+export default config;
--- apps/nextjs/src/app/(marketing)/blog/page.tsx
+++ apps/nextjs/src/app/(marketing)/blog/page.tsx
@@ -1,14 +1,15 @@
-import fs from "fs/promises";
+import fs from "node:fs/promises";
+import path from "node:path";
 
 import { Header } from "../_components/header";
 
 export default async function BlogIndexPage() {
-  const posts = await fs.readdir("./(posts)");
+  const posts = await fs.readdir(path.join(__dirname, "(posts)"));
   return (
     <>
       <Header title="Blog" />
-      {posts.map((post) => (
-        <pre>{JSON.stringify(post, null, 2)}</pre>
+      {posts.map((post, i) => (
+        <pre key={i}>{JSON.stringify(post, null, 2)}</pre>
       ))}
     </>
   );
