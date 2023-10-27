5ab1bdd6ddacbe87fa12893d4a2b62b2345cd572
Samuel Newman <mozzius@protonmail.com>
2023-10-27 08:54:11 +0100

    next 14

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -13,9 +13,6 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
-  experimental: {
-    serverActions: true,
-  },
 };
 
 export default config;
