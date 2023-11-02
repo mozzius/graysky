aaa94178a3ae0f151c6ff159f5b0039e3e234422
Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>
2023-11-02 20:20:38 +0100

    Proper error handling when trying to get an actor's audit log creation date
    Signed-off-by: Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>

--- apps/expo/src/components/screens/profile/profile-info.tsx
+++ apps/expo/src/components/screens/profile/profile-info.tsx
@@ -547,15 +547,17 @@ export const ProfileInfo = ({ profile, backButton }: Props) => {
                 <RichTextWithoutFacets text={profile.description.trim()} />
               </View>
             )}
-          <View className="mt-3">
-            <Text className="text-xs text-neutral-500 dark:text-neutral-400">
-              This user joined Bluesky on {
-                new Intl.DateTimeFormat(locale.languageTag, {
-                  month: "long",
-                  year: "numeric",
-                }).format(profile.createdAt)}
-            </Text>
-          </View>
+          {profile.createdAt && (
+            <View className="mt-3">
+              <Text className="text-xs text-neutral-500 dark:text-neutral-400">
+                This user joined Bluesky on {
+                  new Intl.DateTimeFormat(locale.languageTag, {
+                    month: "long",
+                    year: "numeric",
+                  }).format(profile.createdAt)}
+              </Text>
+            </View>
+          )}
           {profile.viewer?.muted && (
             <View className="mt-3 flex-row items-center justify-between rounded-sm border border-neutral-300 bg-neutral-50 px-2 dark:border-neutral-700 dark:bg-neutral-950">
               <Text className="font-semibold">
