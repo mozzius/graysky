93c527c04376133b38f6783912088af50c1fc2cb
Samuel Newman <mozzius@protonmail.com>
2023-11-28 14:49:38 +0000

    just print out what the push token endpoint recieves

--- apps/nextjs/src/app/api/xrpc/app.bsky.notification.registerPush/route.ts
+++ apps/nextjs/src/app/api/xrpc/app.bsky.notification.registerPush/route.ts
@@ -2,21 +2,10 @@ import { type NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-interface PushNotification {
-  tokens: string[];
-  platform: 1 | 2; // 1 = ios, 2 = android
-  title: string;
-  message: string;
-  topic: string;
-  data?: Record<string, string>;
-  collapse_id?: string;
-  collapse_key?: string;
-}
-
 export async function POST(req: NextRequest) {
-  const { notifications } = (await req.json()) as {
-    notifications: PushNotification[];
-  };
+  const token = await req.json();
 
-  console.log("NOTIFICATIONS", notifications);
+  console.log("NOTIFICATIONS", token);
+
+  return new Response(null, { status: 200 });
 }
