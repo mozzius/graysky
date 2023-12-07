cd3d75af41cadc3a5f5fd89c371f4cd006ee883f
Samuel Newman <mozzius@protonmail.com>
2023-12-07 17:36:03 +0000

    downgrade RN (build errors)

--- apps/expo/scripts/build-android.sh
+++ apps/expo/scripts/build-android.sh
@@ -2,6 +2,6 @@
 
 rm -rf eas-workingdir
 SHORT_SHA=$(git rev-parse --short HEAD)
-rm "$SHORT_SHA.ipa"
+[ -f "$SHORT_SHA.ipa" ] && rm "$SHORT_SHA.ipa"
 EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform android --profile production --non-interactive --local --output="./$SHORT_SHA.ipa" --wait
 [ -f "$SHORT_SHA.ipa" ] && eas submit --platform android --path="./$SHORT_SHA.ipa" --wait
--- apps/expo/scripts/build-ios.sh
+++ apps/expo/scripts/build-ios.sh
@@ -2,7 +2,7 @@
 
 rm -rf eas-workingdir
 SHORT_SHA=$(git rev-parse --short HEAD)
-rm "$SHORT_SHA.ipa"
+[ -f "$SHORT_SHA.ipa" ] && rm "$SHORT_SHA.ipa"
 # EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --non-interactive --local --output="./$SHORT_SHA.ipa" --wait
 EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --local --output="./$SHORT_SHA.ipa" --wait
 [ -f "$SHORT_SHA.ipa" ] && eas submit --platform ios --path="./$SHORT_SHA.ipa" --wait
