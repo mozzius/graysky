d3214e29c5083cf6bcf74e85bf221f61a30d2c5c
Samuel Newman <mozzius@protonmail.com>
2023-12-08 10:07:08 +0000

    non-interactive local submits

--- apps/expo/scripts/build-android.sh
+++ apps/expo/scripts/build-android.sh
@@ -4,4 +4,4 @@ rm -rf eas-workingdir
 SHORT_SHA=$(git rev-parse --short HEAD)
 [ -f "$SHORT_SHA.ipa" ] && rm "$SHORT_SHA.ipa"
 EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform android --profile production --non-interactive --local --output="./$SHORT_SHA.ipa" --wait
-[ -f "$SHORT_SHA.ipa" ] && eas submit --platform android --path="./$SHORT_SHA.ipa" --wait
+[ -f "$SHORT_SHA.ipa" ] && eas submit --platform android --path="./$SHORT_SHA.ipa" --non-interactive --wait
--- apps/expo/scripts/build-ios.sh
+++ apps/expo/scripts/build-ios.sh
@@ -3,6 +3,6 @@
 rm -rf eas-workingdir
 SHORT_SHA=$(git rev-parse --short HEAD)
 [ -f "$SHORT_SHA.ipa" ] && rm "$SHORT_SHA.ipa"
-# EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --non-interactive --local --output="./$SHORT_SHA.ipa" --wait
-EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --local --output="./$SHORT_SHA.ipa" --wait
-[ -f "$SHORT_SHA.ipa" ] && eas submit --platform ios --path="./$SHORT_SHA.ipa" --wait
+EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --non-interactive --local --output="./$SHORT_SHA.ipa" --wait
+# EAS_LOCAL_BUILD_WORKINGDIR="$PWD/eas-workingdir" eas build --platform ios --profile production --local --output="./$SHORT_SHA.ipa" --wait
+[ -f "$SHORT_SHA.ipa" ] && eas submit --platform ios --path="./$SHORT_SHA.ipa" --non-interactive --wait
