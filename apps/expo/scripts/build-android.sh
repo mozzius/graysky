a0875a6528e42ff08b4f4b1346c631ce4c48cc49
Samuel Newman <mozzius@protonmail.com>
2023-12-09 00:13:17 +0000

    removed args from wrong command

--- apps/expo/scripts/build-android.sh
+++ apps/expo/scripts/build-android.sh
@@ -9,5 +9,5 @@ export $(xargs < ../../.env)
 export GOOGLE_SERVICES_JSON=$(get_abs_filename google-services.json)
 SHORT_SHA=$(git rev-parse --short HEAD)
 eas build --platform android --profile production --non-interactive --local --output="./$SHORT_SHA.aab" --wait
-[ -f "$SHORT_SHA.aab" ] && eas submit --platform android --path="./$SHORT_SHA.aab"
+[ -f "$SHORT_SHA.aab" ] && eas submit --platform android --path="./$SHORT_SHA.aab" --non-interactive --wait
 [ -f "$SHORT_SHA.aab" ] && rm "$SHORT_SHA.aab"
