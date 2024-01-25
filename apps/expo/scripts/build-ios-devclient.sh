1382b6e7031a98ee1b153cff7f094bcd176eb13f
Samuel Newman <mozzius@protonmail.com>
2024-01-25 18:03:21 +0000

    change ios sim build script to bash script

---
+++ apps/expo/scripts/build-ios-devclient.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export $(xargs < ../../.env)
+eas build --platform ios --profile development-simulator --non-interactive --local --output=\"./graysky.tar.gz\" --wait
+[ -f "graysky.tar.gz" ] &&  eas build:run -p ios --path=\"graysky.tar.gz\""
