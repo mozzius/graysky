659886c43d622fe92d0ae5ffec3b8631705473d3
Samuel Newman <mozzius@protonmail.com>
2024-02-02 01:16:37 +0000

    use redis url if available

--- apps/push-notifs/src/db.ts
+++ apps/push-notifs/src/db.ts
@@ -1,7 +1,21 @@
-import { createClient } from "redis";
+import {
+  createClient,
+  type RedisClientOptions,
+  type RedisFunctions,
+  type RedisModules,
+  type RedisScripts,
+} from "redis";
+
+let options: RedisClientOptions<RedisModules, RedisFunctions, RedisScripts>;
+
+if (process.env.REDIS_URL) {
+  options = {
+    url: process.env.REDIS_URL,
+  };
+}
 
 export const getRedisClient = async () => {
-  return await createClient()
+  return await createClient(options)
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 };
