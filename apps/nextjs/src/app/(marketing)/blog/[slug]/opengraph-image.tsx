0a040c4bf1a1b969af24efc6221f0cb5dd827a68
Samuel Newman <mozzius@protonmail.com>
2024-02-05 15:18:10 +0000

    update deps

--- apps/nextjs/src/app/(marketing)/blog/[slug]/opengraph-image.tsx
+++ apps/nextjs/src/app/(marketing)/blog/[slug]/opengraph-image.tsx
@@ -10,6 +10,7 @@ export const size = {
   width: 1200,
   height: 630,
 };
+
 export const contentType = "image/png";
 
 export default async function Image({ params }: { params: { slug: string } }) {
