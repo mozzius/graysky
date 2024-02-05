d2ad905a52c772e6a13d88cc1368cab6f256e920
Samuel Newman <mozzius@protonmail.com>
2024-02-05 14:12:14 +0000

    add fonts to og image

--- apps/nextjs/src/app/(marketing)/blog/[slug]/opengraph-image.tsx
+++ apps/nextjs/src/app/(marketing)/blog/[slug]/opengraph-image.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/og";
 
@@ -12,40 +13,78 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image({ params }: { params: { slug: string } }) {
-  const blogDataPromise = fetch(
-    `https://graysky.app/blog/${params.slug}/info`,
-  ).then((res) => res.json() as Promise<BlogData>);
+  try {
+    const blogDataPromise = fetch(
+      `https://graysky.app/blog/${params.slug}/info`,
+    ).then((res) => res.json() as Promise<BlogData>);
 
-  const imagePromise = fetch(
-    new URL("~/assets/graysky.jpg", import.meta.url),
-  ).then((res) => res.arrayBuffer());
+    const imagePromise = fetch(
+      new URL("~/assets/graysky.jpg", import.meta.url),
+    ).then((res) => res.arrayBuffer());
 
-  const [blogData, image] = await Promise.all([blogDataPromise, imagePromise]);
+    const interRegularFontPromise = fetch(
+      new URL("~/assets/fonts/Inter-Regular.ttf", import.meta.url),
+    ).then((res) => res.arrayBuffer());
 
-  const formatter = new Intl.DateTimeFormat("en-GB", {
-    dateStyle: "medium",
-  });
+    const interBoldFontPromise = fetch(
+      new URL("~/assets/fonts/Inter-Bold.ttf", import.meta.url),
+    ).then((res) => res.arrayBuffer());
 
-  return new ImageResponse(
-    (
-      <div tw="w-full h-full flex">
-        {image && (
-          <img // @ts-expect-error img behaves differently here
-            src={image}
-            height="1200"
-            width="630"
-            style={{ objectFit: "cover" }}
-            alt=""
-          />
-        )}
-        <div tw="p-8 absolute flex bottom-0 w-full left-0 justify-end h-full bg-neutral-950/20">
-          <h1 tw="text-4xl font-bold">{blogData.title}</h1>
-          <p tw="text-lg">
-            {formatter.format(new Date(blogData.date))} - by {blogData.author}
-          </p>
+    const [blogData, image, interRegularFont, interBoldFont] =
+      await Promise.all([
+        blogDataPromise,
+        imagePromise,
+        interRegularFontPromise,
+        interBoldFontPromise,
+      ]);
+
+    const formatter = new Intl.DateTimeFormat("en-GB", {
+      dateStyle: "medium",
+    });
+
+    return new ImageResponse(
+      (
+        <div tw="w-full h-full flex">
+          {image && (
+            <img // @ts-expect-error img behaves differently here
+              src={image}
+              height="1200"
+              width="630"
+              tw="w-full relative -top-1/2"
+              alt=""
+            />
+          )}
+          <div tw="bg-neutral-900 opacity-60 w-full h-full absolute top-0 left-0" />
+          <div tw="p-16 absolute flex flex-col bottom-0 w-full left-0 justify-end h-full">
+            <h1 tw="text-7xl font-bold text-white">{blogData.title}</h1>
+            <p tw="text-4xl text-neutral-300">
+              {formatter.format(new Date(blogData.date))} - by {blogData.author}
+            </p>
+          </div>
         </div>
-      </div>
-    ),
-    { ...size },
-  );
+      ),
+      {
+        ...size,
+        fonts: [
+          {
+            name: "Inter",
+            data: interRegularFont,
+            style: "normal",
+            weight: 400,
+          },
+          {
+            name: "Inter",
+            data: interBoldFont,
+            style: "normal",
+            weight: 700,
+          },
+        ],
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
