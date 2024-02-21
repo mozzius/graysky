bad4d94f211ce1255b2d653f2a3dba79fa1f08b5
Samuel Newman <mozzius@protonmail.com>
2024-02-21 23:39:00 +0000

    Revert "try prisma edge runtime"
    This reverts commit 5bf0c46733051b2f32866462875a8947ae4f78a6.

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,18 +1,12 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 export * from "@prisma/client";
 
 const globalForPrisma = globalThis as { prisma?: PrismaClient };
 
-const client = new Client({ url: process.env.DATABASE_URL });
-const adapter = new PrismaPlanetScale(client);
-
 export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
