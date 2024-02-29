a42cc30e638c9a97cf7acc0e4acf10ce7f59e4d3
Samuel Newman <mozzius@protonmail.com>
2024-02-29 11:19:28 +0000

    remove unused string

--- apps/expo/lingui.config.js
+++ apps/expo/lingui.config.js
@@ -1,6 +1,20 @@
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ["en", "ja", "es", "fr", "de", "be", "pt", "nl", "cs", "ml", "uk"],
+  locales: [
+    "en",
+    "ja",
+    "es",
+    "fr",
+    "de",
+    "be",
+    "pt",
+    "nl",
+    "cs",
+    "ml",
+    "uk",
+    "ar",
+    "hr",
+  ],
   catalogs: [
     {
       path: "<rootDir>/src/i18n/locales/{locale}/messages",
